14015b072c85b4b032c617d09bf493c5fe3b7035
Drishta Grover <drishtagrover13781@gmail.com>
2025-08-17 13:30:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import './App.css'
-import MenuBar from './components/MenuBar';
-import LandingPage from './pages/LandingPage/LandingPage';
-import Dashboard from './pages/Dashboard';
-import MainPage from './pages/MainPage';
-import PreviewPage from './pages/PreviewPage';
-import UserSyncHandler from './components/UserSyncHandler';
+import MenuBar from './components/MenuBar.jsx';
+import LandingPage from './pages/LandingPage/LandingPage.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import MainPage from './pages/MainPage.jsx';
+import PreviewPage from './pages/PreviewPage.jsx';
+import UserSyncHandler from './components/UserSyncHandler.jsx';
 import { RedirectToSignIn, SignedIn, SignedOut } from '@clerk/clerk-react';
 
 function App() {
